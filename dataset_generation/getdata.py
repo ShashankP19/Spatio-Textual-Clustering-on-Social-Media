3cc7b6fbbda1315d7b6546d1b4012ecdacaaf1e7
Praveen Gupta <pvgupta24@gmail.com>
2019-10-01 12:04:27 +0530

    Updates get-data script for multiple keywords at once

--- dataset_generation/getdata.py
+++ dataset_generation/getdata.py
@@ -2,46 +2,45 @@ import os
 import csv
 import tweepy as tw
 import pandas as pd
+import sys
 
-# Edit this with your keys
-consumer_key= "<CONSUMER_KEY>"
-consumer_secret= "<CONSUMER_SECRET_KEY>"
-access_token= "<ACCESS_TOKEN>"
-access_token_secret= "<ACCESS_TOKEN_SECRET>"
+from twitter_config import *
+
+DATA_DIR = 'data/'
+NO_OF_TWEETS = 200
 
 auth = tw.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tw.API(auth, wait_on_rate_limit=True)
 
+if len(sys.argv) < 2:
+	print('Usage: python3 ' + sys.argv[0] + ' <Space separated list of keywords to search for>')
+
+os.makedirs(DATA_DIR, exist_ok=True)
 
 # Define the search term and the date_since date as variables
-search_words = 'Hyde Park'
-date_since = "2016-05-30"
-date_until = "2019-09-26"
+search_words = sys.argv[1:]
+# ['Edinburgh Castle', 'edinburgh castle', 'EdinburghCastle']
 
-# Create open csv file to write data
-f = open('collected_tweets.csv', 'w', encoding="utf-8")
-csv_file=csv.writer(f)
 columns = ['created_at', 'id_str', 'text', 'longitude', 'latitude']
-csv_file.writerow(columns)
+
+for word in search_words:
+	print('Searching for tweets containing ' + word)
+	# Create open csv file to write data
+	f = open(DATA_DIR + word.replace(' ', '-') + '.csv', 'w', encoding="utf-8")
+	csv_file=csv.writer(f)
+	csv_file.writerow(columns)
 
 
-# Collect tweets
-tweets = tw.Cursor(api.search,
-              q=search_words,
-              lang="en",
-              until=date_until,
-              since=date_since).items(20)
+	# Collect tweets
+	tweets = tw.Cursor(api.search,
+		        q=search_words,
+	            lang="en"
+			  ).items(NO_OF_TWEETS)
 
-# Store in csv file
-for tweet in tweets:
-    if tweet.coordinates != None:
-        longitude, latitude = tweet.coordinates["coordinates"]
-        csv_file.writerow([tweet.created_at, tweet.id_str, tweet.text, longitude, latitude])
-
-    # else:
-    #     longitude = 'None'
-    #     latitude = 'None'
-    
-    # csv_file.writerow([tweet.created_at, tweet.id_str, tweet.text, longitude, latitude])
+	# Store in csv file
+	for tweet in tweets:
+		if tweet.coordinates != None:
+		    longitude, latitude = tweet.coordinates["coordinates"]
+		    csv_file.writerow([tweet.created_at, tweet.id_str, tweet.text, longitude, latitude])
 
---
+++ dataset_generation/twitter_config_template.py
@@ -0,0 +1,6 @@
+
+# Add twitter app keys here
+consumer_key= ""
+consumer_secret= ""
+access_token= ""
+access_token_secret= ""
