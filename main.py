d1e492a54dee30cd533b65848a23c939487c1845
Praveen Gupta <pvgupta24@gmail.com>
2019-11-05 23:55:10 +0530

    Plots cluster on world map

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from shapely.geometry import Point
 import geopandas as gpd
 from geopandas import GeoDataFrame
 import matplotlib.pyplot as plt
+import matplotlib.colors as mpl_colors
 
 
 def main():
@@ -27,31 +28,27 @@ def main():
     ll_irrelevant_values = ll_irrelevant.values
     # print(ll_irrelevant_values[:5])
 
-    eps = 1
-    N_min = 5
+    eps = 5
+    N_min = 3
     N_max = 10
     clusters = dbtexc(ll_relevant_values,
                       ll_irrelevant_values, eps, N_min, N_max)
 
-
     world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-    markersize = 15
-    ax = world.plot(figsize=(10, 6))
+    markersize = 2
     colors = plt.get_cmap('jet')(np.linspace(0.0, 1.0, len(clusters)))
-    print(colors)
-    print(len(clusters))
+    ax = world.plot(figsize=(20, 12), color='#e8e4b3')
+
     for cluster_id, cluster in enumerate(clusters):
         df = pd.DataFrame(cluster)
         geometry = [Point(p) for p in zip(df[0], df[1])]
         gdf = GeoDataFrame(df, geometry=geometry)
-        gdf.plot(ax=ax, marker='o', color=colors[cluster_id], markersize=markersize)
-                
-    # df = pd.read_csv("dataset.csv", delimiter=',',
-    #                  skiprows=0, low_memory=False)
-
-    # geometry = [Point(xy) for xy in zip(df['longitude'], df['latitude'])]
-    # gdf = GeoDataFrame(df, geometry=geometry)
-
+        gdf.plot(ax=ax, marker='o', color=mpl_colors.to_hex(
+            colors[cluster_id]), markersize=markersize, label=('{} points'.format(len(cluster))))
+    
+    plt.axis('off')
+    plt.legend(loc='best')
+    plt.suptitle('DBTexC Clusters on Twitter Data', fontsize=16)
     plt.show()
 
 
