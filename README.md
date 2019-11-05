876e34fe732a9147097390b2edb467d8734c187f
Praveen Gupta <pvgupta24@gmail.com>
2019-11-05 15:28:20 +0530

    Adds expandCluster in dbtexc

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Spatio-Textual-Clustering-on-Social-Media
-Implementation of the paper DBSTexC: Density-Based Spatio–Textual Clustering on Twitter
+Implementation of the paper [DBSTexC: Density-Based Spatio–Textual Clustering on Twitter](https://dl.acm.org/citation.cfm?id=3110096)
+
+## References
+- [DBSTexC: Density-Based Spatio–Textual Clustering on Twitter](https://dl.acm.org/citation.cfm?id=3110096)
+- [DBScan Simple Implementation](https://github.com/chrisjmccormick/dbscan)
--- dbtexc/dbtexc.py
+++ dbtexc/dbtexc.py
@@ -5,128 +5,101 @@ import numpy
 NOISE = -1
 UNCLUSTERED = 0
 
-def dbtexc(m, eps, min_points, n_min, n_max):
-	"""Implementation of DBSTexC: Density-Based Spatio-Textual Clustering on Twitter
-	See https://dl.acm.org/citation.cfm?id=3110096
-		Uses Euclidean Distance as the measure
-		Inputs:
-	m - A matrix whose columns are feature vectors, each row is a point
-	eps - Maximum distance two points can be to be regionally related
-	n_min - The minimum number of points to make a cluster
-		Outputs:
-	An array with either a cluster id number or dbscan.NOISE for each
-	column vector in m.
+# Labels for marking the points once it is visited
+VISITED = True
+UNVISITED = False
+
+
+def dbtexc(X, Y, eps, n_min, n_max):
     """
-	    labels = [UNCLUSTERED]*len(m)
-	    current_cluster = 0 # Current Cluster ID
-			P  n range(0, len(m)):
-		   (labels[P] != UNCLUSTERED):
+    Implementation of DBSTexC: Density-Based Spatio-Textual Clustering on Twitter
+    See https://dl.acm.org/citation.cfm?id=3110096
+
+    Uses Euclidean Distance as the measure
+
+    Inputs:
+    X - A matrix whose columns are feature vectors, each row is a relevant point
+    Y - A matrix whose columns are feature vectors, each row is a ir    relevant point
+    eps - Maximum distance two points can be to be regionally related
+    n_min - The minimum number of points to make a cluster
+
+    Outputs:
+    An array with either a cluster id number or dbscan.NOISE for each
+    column vector in m.
+    """
+
+    # Initialization
+    n = len(X)
+    m = len(Y)
+    current_cluster = 0
+
+    labels_X = [UNCLUSTERED]*n
+    visited_X = [UNVISITED]*n
+
+    labels_Y = [UNCLUSTERED]*m
+    visited_Y = [UNVISITED]*m
+
+    for point in range(0, n):
+        if visited_X[point] == VISITED:
             continue
-		        relevant_neighbours, irrelevant_neighbours = regionQuery(m, P, eps)
-				# TODO: Add condition checks for Noise
-			len(relevant_neighbours) >= n_min and len(irrelevant_neighbours) <= n_max):
-			# grcurrent_cluster += 1
-        #    growCluster(m, labels, P, neighbours, current_cluster, eps, min_points)
-	    # All data has been clustered!
-    return labels
-	for P in range(0, len(relevant_neighbours)):
-		if(labels[P] != UNCLUSTERED):
-			continue
-		
-def growCluster(m, labels, P, neighbours, current_cluster, eps, current_cluster, min_points):
-    """
-	Grow a new cluster with label current_cluster from the seed point P.
-	
-	This function sParches through the dataset to find all point, current_clusters that belong
-	to tid, pointis nenumerateWhen this function rturns, cluster current_cluster is complete.
-	
-point	Parameters
-		
-		rel_nb, irrel_nb = regionQuery(m, point, eps)
-		if(len(relevant_neighbours) >= n_min and len(irrelevant_neighbours) <= n_max):
-				relevant_neighbors = relevant_neighbors.union(rel_nb)
-				irrelevant_neighbors = relevant_neighbors.union(irrel_nb)
-		labels[point] = current_cluster:
-	
-	if len(irrelevant_neighbours) != 0 :
-		for id, point in enumerate(irrelevant_neighbours):
-			if labels[point] == UNCLUSTERED:
-				labels[point] = current_cluster
-ls - List storing the cluster labels for all dataset points
-	  P      - Index of the seed point for this new cluster
-	  neighbours - All of the neighbors of P
-	  current_cluster      - The label for this new cluster.  
-		s    - Threshold distance
-		n_points - Minimum required number of neighbors
-		
-		   			 the cluster label to the seed point.
-	labels[P] = current_cluster
-	
-    # Look at each neighbor of P (neighbors are referred to as Pn). 
-    # neighbours will be used as a FIFO queue of points to search--that is, it
-    # will grow as we discover new branch points for the cluster. The FIFO
-    # behavior is accomplished by using a while-loop rather than a for-loop.
-    # In neighbours, the points are represented by their index in the original
-    # dataset.
-    """
-    i = 0
-    while i < len(neighbours):    
-        
-        # Get the next point from the queue.        
-        Pn = neighbours[i]
-       
-        # If Pn was labelled NOISE during the seed search, then we
-        # know it's not a branch point (it doesn't have enough neighbors), so
-        # make it a leaf point of cluster current_cluster and move on.
-        if labels[Pn] == -1:
-           labels[Pn] = current_cluster
-        
-        # Otherwise, if Pn isn't already claimed, claim it as part of current_cluster.
-        elif labels[Pn] == 0:
-            # Add Pn to cluster current_cluster (Assign cluster label current_cluster).
-            labels[Pn] = current_cluster
-            
-            # Find all the neighbors of Pn
-            PnNeighborPts = regionQuery(m, Pn, eps)
-            
-            # If Pn has at least min_points neighbors, it's a branch point!
-            # Add all of its neighbors to the FIFO queue to be searched. 
-            if len(PnNeighborPts) >= min_points:
-                neighbours = neighbours + PnNeighborPts
-            # If Pn *doesn't* have enough neighbors, then it's a leaf point.
-            # Don't queue up it's neighbors as expansion points.
-            #else:
-                # Do nothing                
-                #neighbours = neighbours               
-        
-        # Advance to the next point in the FIFO queue.
-        i += 1        
-    
-    # We've finished growing cluster current_cluster!
+
+        visited_X[point] = VISITED
+
+        eps_X, eps_Y = regionQuery(X, Y, point, eps)
+
+        if(len(eps_X) >= n_min and len(eps_Y) <= n_max):
+            current_cluster += 1
+            expandCluster(point, eps_X, eps_Y, visited_X, visited_Y,
+                          labels_X, labels_Y, X, Y, current_cluster, eps, n_min, n_max)
+
+    return labels_X, labels_Y
+
+
+def expandCluster(central_point, eps_X, eps_Y, visited_X, visited_Y, labels_X, labels_Y, X, Y, current_cluster, eps, n_min, n_max):
+    labels_X[central_point] = current_cluster
+
+    index = 0
+    while index < len(eps_X):
+        point = eps_X[index]
+        index += 1
+
+        if visited_X[point] == VISITED:
+            continue
+        visited_X[point] = VISITED
+
+        rel, irrel = regionQuery(X, Y, point, eps)
+        if(len(rel) >= n_min and len(irrel) <= n_max):
+            eps_X += rel
+            eps_Y += irrel
+        if labels_X[point] == UNCLUSTERED:
+            labels_X[point] = current_cluster
+
+    if len(eps_Y) != 0:
+        for point in eps_Y:
+            if visited_Y[point] == UNVISITED:
+                visited_Y[point] = VISITED
+                if labels_Y[point] == UNCLUSTERED:
+                    labels_Y[point] = current_cluster
 
 
 def regionQuery(X, Y, P, eps):
     """
     Find all points in dataset X and Y within distance eps of point P.
-    
-    This function calculates the distance between a point P and
- every other 
+
+    This function calculates the distance between a point P and every other 
     point in the dataset, and then returns only those points which are within a
     threshold distance eps.
     """
-    relevant_neighbors = []
-    irrelevant_neighbors = []
 
-    # Find relevant neighbors
-    for q in range(0, len(X)):
-        # If the distance is below the threshold, add it to the relevant neighbors list.
-        if numpy.linalg.norm(X[P] - X[q]) < eps:
-           relevant_neighbors.append(q)
-    
-    # Find irrelevant neighbors
-    for q in range(0, len(Y)):
-        # If the distance is below the threshold, add it to the irrelevant neighbors list.
-        if numpy.linalg.norm(X[P] - Y[q]) < eps:
-           irrelevant_neighbors.append(q)
-            
-    return relevant_neighbors, irrelevant_neighbors
+    return findNeighbours(X, P, eps), findNeighbours(Y, P, eps)
+
+
+def findNeighbours(points, central_point, eps):
+    neighbours = []
+
+    for q in range(0, len(points)):
+        # If the distance is below the threshold, add it to the neighbors list
+        if numpy.linalg.norm(points[central_point] - points[q]) < eps:
+            neighbours.append(q)
+
+    return neighbours
